6fd98be96b486f9945e54f4bf794e940f200acab
Renz <renzcarillo32@gmail.com>
2022-01-15 07:55:22 +0800

    feat: add database for server

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,4 @@
+// TODO - fix fetching api, add heatmap layer
 import React, { useState, useEffect } from "react";
 import {
   withGoogleMap,
@@ -10,45 +11,75 @@ import {
 // import mapStyles from "./mapStyles";
 
 function Map() {
-  const [selectedPark, setSelectedPark] = useState(null);
+  const [pharmacyList, setPharmacyList] = useState(null);
 
+  // using escape from keyboard to make the popup go away 
   useEffect(() => {
     const listener = e => {
       if (e.key === "Escape") {
-        setSelectedPark(null);
+        setPharmacyList(null);
       }
     };
-    window.addEventListener("keydown", listener);
+    window.addEventListener("keydown", listener); // keydown is for a keyboard key
 
     return () => {
       window.removeEventListener("keydown", listener);
     };
-  }, []);
+  }, []); // blank array means only do this once 
+
+  /* TODO : 
+    fix errors in cors policy
+    add location bias
+    make the result into multiple candidates
+  */
+
+  // fetching data - grab the type, it must be equal to pharmacy 
+  useEffect( () => {
+    const fetchDataForPharmacy = async () => {
+      try {
+        const res = await fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&fields=formatted_address%2Cname%2Ctypes%2Cplace_id%2Cgeometry&input=pharmacy&inputtype=textquery&key=AIzaSyDBqerYcBLd6bAM2S_GAmLejYH_xVNNa-U`);
+        console.log(res);
+        const data = await res.json();
+
+        // at query above
+        //&locationbias=circle%3A2000%14.1153%120.9621 
+        
+        console.log(data);
+        // setPharmacyList(data);
+        // console.log(pharmacyList.candidates[0].position);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchDataForPharmacy();
+  },[]);
 
   return (
     <GoogleMap
       defaultZoom={10}
-      defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
+      defaultCenter={{ lat: 14.1153, lng: 120.9621}}
       //defaultOptions={{ styles: mapStyles }}
     >
-      {/* parkData.features.map(park => (
+      {/* for displaying result */}
+      {/* {pharmacyList.candidates.map(pharmacy => (
         <Marker
-          key={park.properties.PARK_ID}
+          key={pharmacy.places_id}
           position={{
-            lat: park.geometry.coordinates[1],
-            lng: park.geometry.coordinates[0]
+            lat: pharmacy.location.lat,
+            lng: pharmacy.location.lng
           }}
-          onClick={() => {
-            setSelectedPark(park);
-          }}
-          icon={{
-            url: `/skateboarding.svg`,
-            scaledSize: new window.google.maps.Size(25, 25)
-          }}
+          // onClick={() => {
+          //   setSelectedPark(park);
+          // }}
+          // icon={{
+          //   url: `/skateboarding.svg`,
+          //   scaledSize: new window.google.maps.Size(25, 25)
+          // }}
         />
-        )) */} 
+        )) }  */}
 
-      {selectedPark && (
+      {/* for creating the popup */}
+      {/* selectedPark && (
         <InfoWindow
           onCloseClick={() => {
             setSelectedPark(null);
@@ -63,7 +94,7 @@ function Map() {
             <p>{selectedPark.properties.DESCRIPTIO}</p>
           </div>
         </InfoWindow>
-      )}
+        ) */}
     </GoogleMap>
   );
 }
@@ -74,9 +105,10 @@ export default function Home() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.GOOGLE_API_KEY
-        }`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=
+        AIzaSyDBqerYcBLd6bAM2S_GAmLejYH_xVNNa-U`
+        //${process.env.REACT_APP_GOOGLE_KEY}
+        }
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
